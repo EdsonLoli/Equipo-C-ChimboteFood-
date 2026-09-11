dbd7866e8a796db777f484b9b544efc1a31e7280
EdsonLoli <U22216812@utp.edu.pe>
2026-09-11 01:36:36 -0500

    Detalle de diseño

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actualizar inventario</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="diseño.css">
 </head>
 <body>
     <main class="page-shell">
